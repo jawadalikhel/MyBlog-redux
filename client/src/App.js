9a4752ab2d8f64ea9069b96a97b26c5d1a220c81
Your Name <jawadalikhel18@gmail.com>
2019-03-28 13:27:25 -0500

    created the store and rootreducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import LandingPage from './components/';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <LandingPage />
       </div>
     );
   }
